95c972d9f784ca848be97fa79f799dae145051dc
Sophie Holland <sophieholland236@gmail.com>
2025-04-09 11:14:24 -0400

    Added jump function and gravity

--- js/player.js
+++ js/player.js
@@ -19,16 +19,34 @@ export default class Player {
       x: x,
       y: y
     }
+    //velocity is speed in pixels
     this.velocity = {
       x: 0,
       y: 0
     };
   }
 
+  //getters to get math value for right side and bottom (to stop from going off screen)
+  get right(){ return this.position.x + this.width }
+  get bottom(){ return this.position.y + this.height }
+  get left(){return this.position.x}
+  get top(){return this.position.y}
+
   /**
    * Main function to update location, velocity, and image
    */
   update() {
+    //add gravity to the hero
+    this.velocity.y += GRAVITY
+
+    //if we hit the floor, stop falling
+    if(this.bottom > FLOOR){
+      this.velocity.y = 0
+      this.position.y = FLOOR - this.height
+    }
+    //Update the location of the hero
+    this.position.x += this.velocity.x
+    this.position.y += this.velocity.y
     this.draw();
   }
 
@@ -38,5 +56,10 @@ export default class Player {
   draw() {
     CTX.fillStyle = "yellow";
     CTX.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+
+  jump(){
+    this.position.y -= 1
+    this.velocity.y = -20
   }
 }
